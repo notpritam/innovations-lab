982b1c1552187de1d84988c38074c4ccc8763f65
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-02 23:22:02 +0530

    3d Package

--- app/page.js
+++ app/page.js
@@ -1,5 +1,23 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="w-screen mt-[4rem]">Content</main>;
+  return (
+    <main className="w-screen mt-[4rem]">
+      <div className="hero p-4  flex flex-col gap-8 w-full">
+        <div className="flex flex-col gap-2">
+          <span className="text-[2rem] font-bold">
+            Create. Print. Innovate.
+          </span>
+          <span>
+            Unleashing Innovation in Every Layer .<br />
+            Get your 3D assets printed. Showcase your 3D work. Buy & sell 3D
+            models.
+          </span>
+        </div>
+        <div>
+          <img src="/hero.svg" alt="hero" />
+        </div>
+      </div>
+    </main>
+  );
 }
