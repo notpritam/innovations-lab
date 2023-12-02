be6813373c3c02e08855b4e668682d5f39ebaf9f
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-02 22:23:55 +0530

    Header

---
+++ components/header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+
+function Header() {
+  return (
+    <div
+      data-mode="dark"
+      className="w-full pl-4 pr-4 fixed justify-between top-0 left-0 right-0 h-[4rem] flex items-center bg:dark"
+    >
+      <div>
+        <Button variant="outline" size="icon">
+          <RiMenu2Line className="h-4 w-4" />
+        </Button>
+        <Link className="text-xl" href="/">
+          Innovations Lab
+        </Link>
+      </div>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+}
+
+export default Header;
